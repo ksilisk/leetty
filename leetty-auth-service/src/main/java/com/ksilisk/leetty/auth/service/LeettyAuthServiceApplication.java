86122802d664392568b9c007269e6aee81494cb3
Shaliko Salimov <shaliko9@yandex.ru>
2024-01-09 20:17:58 +0300

    feat: implement oauth2 authorization between services

---
+++ leetty-auth-service/src/main/java/com/ksilisk/leetty/auth/service/LeettyAuthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ksilisk.leetty.auth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeettyAuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeettyAuthServiceApplication.class, args);
+    }
+
+}
--- leetty-telegram-bot/src/main/java/com/ksilisk/leetty/telegram/bot/LeettyTelegramBotApplication.java
+++ leetty-telegram-bot/src/main/java/com/ksilisk/leetty/telegram/bot/LeettyTelegramBotApplication.java
@@ -2,12 +2,15 @@ package com.ksilisk.leetty.telegram.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LeettyTelegramBotApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(LeettyTelegramBotApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(LeettyTelegramBotApplication.class, args);
+        String test = run.getBean("test", Test.class).demoClient.hello();
+        System.out.println(test);
     }
-
 }
